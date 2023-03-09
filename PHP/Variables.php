160595bb841e2ae8fe17b257f56afbde4afea904
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-03-09 14:18:15 +0200

    PHP

---
+++ PHP/Variables.php
@@ -0,0 +1,28 @@
+<?php 
+    // int
+    $number = 2023;
+    echo $number;
+    
+    // String
+    $text = " Mohamad \n";
+    echo "<h1>$text</h1>";
+
+    // float - double
+    $float_number = 100.25;
+    echo $float_number;
+
+    // boolean
+    $boolean = true;
+    echo $boolean;
+
+    // null
+    $name = null;
+    $name = "Mohamad";
+    echo "<p>$name</p>";
+    
+    // array 
+    $years = array(2020,2021,2022,2023);
+    echo $years[1];
+    
+    // object
+?>
