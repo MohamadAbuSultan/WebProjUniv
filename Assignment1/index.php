c58ae52a940b282318eb211de2f48e397e6d0fcb
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-05-28 15:07:23 +0300

    .

--- Assignment1/index.php
+++ Assignment1/index.php
@@ -9,13 +9,14 @@
             list-style: none;
             text-decoration: none;
             font-weight: bold;
+            background-color: rgb(228, 255, 242);
         }
 
         header nav li {
             display: table;
             height: 100%;
             float: left;
-            margin: 30px 50px;
+            margin: 30px 40px;
         }
 
         header nav a:hover {
@@ -24,6 +25,7 @@
 
         nav a {
             color: black;
+            background-color: #0080FF;
         }
 
         header {
@@ -39,21 +41,29 @@
 </head>
 
 <body>
+    <div>
+        <header>
+            <div>
+                <nav>
+                    <li><span><a href="">My Page</a></span></li>
+                    <li><span><a href="">Employee Engagment</a></span></li>
+                    <li><span><a href="">Employee Directory</a></span></li>
+                    <li><span><a href="">PMS</a></span></li>
+                    <li><span><a href="">ESS</a></span></li>
+                    <li><span><a href="">Dashboard</a></span></li>
+                    <li><span><a href="">Downloads</a></span></li>
+                </nav>
+            </div>
+        </header>
 
-    <header>
-        <div>
-            <nav>
-                <li><span><a href="">My Page</a></span></li>
-                <li><span><a href="">Employee Engagment</a></span></li>
-                <li><span><a href="">Employee Directory</a></span></li>
-                <li><span><a href="">PMS</a></span></li>
-                <li><span><a href="">ESS</a></span></li>
-                <li><span><a href="">Dashboard</a></span></li>
-                <li><span><a href="">Downloads</a></span></li>
-            </nav>
+        <div style="background-color: #00FFFF; margin: 20px;">
+            Attendence Regularisation Form <br> <br>
+                  <input type="radio" id="Self" name="Attendence" value="Self" style="margin-left: 500px;">
+                  <label for="Self">Self</label>
+                  <input type="radio" id="OnBehalf" name="Attendence" value="OnBehalf">
+                  <label for="OnBehalf">On Behalf</label>
         </div>
-    </header>
-
+    </div>
 </body>
 
 </html>
